31835e23cefe18f91615a4ef8cf5b469023a7415
Bob Jeltes <bobjeltes@hotmail.com>
2018-02-16 13:37:51 +0100

    Unity Project
    Project aangemaakt

---
+++ dingprojectenzo/Assets/Standard Assets/PhysicsMaterials/helicopter.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class helicopter : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (transform.position.y < 100) {
+			transform.position = new Vector3(transform.position.x, transform.position.y + .1f, transform.position.z);
+		}
+		else if (transform.position.y > 100) {
+			transform.position = new Vector3(transform.position.x, transform.position.y - .1f, transform.position.z);
+
+		}
+		transform.eulerAngles = new Vector3 (transform.eulerAngles.x, transform.eulerAngles.y + 100f, transform.eulerAngles.z);
+	}
+}
